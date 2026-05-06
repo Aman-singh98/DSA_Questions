463be00f04d1e0994a7f8727727a34195a677f11
Aman Singh <amansingh160798@gmail.com>
2026-05-06 12:28:18 +0530

    Complete array basic problems

--- Array/helloWorld.js
+++ Array/helloWorld.js
@@ -89,12 +89,165 @@ const searchElement = 5;
 // Output: 100
 
 function largest(arr) {
-   return arr;
+   let max = arr[0];
+   for (i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+         max = arr[i];
+      }
+   }
+   return max;
 }
 
 function smallest(arr) {
-   return arr;
+   let min = arr[0];
+   for (i = 0; i < arr.length; i++) {
+      if (arr[i] < min) {
+         min = arr[i];
+      }
+   }
+   return min;
 }
 
 const maxAndMinArr = [20, 10, 20, 4, 100];
-console.log(largest(maxAndMinArr), smallest(maxAndMinArr), "Largest and smallest value:");
+// console.log(largest(maxAndMinArr), smallest(maxAndMinArr), "Largest and smallest value:");
+
+// Basic problem 4:Second Largest Element in an Array
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+
+function secondLargest(arr) {
+   let secondLargest = -1;
+   let largest = -1;
+   for (i = 0; i < arr.length; i++) {
+      if (arr[i] > largest) {
+         secondLargest = largest;
+         largest = arr[i];
+      }
+      if (arr[i] < largest && arr[i] > secondLargest) {
+         secondLargest = arr[i];
+      }
+   }
+
+   return { secondLargest, largest };
+}
+
+const secondLargeArr = [12, 30, 35, 10, 34, 1, 70, 71]
+// console.log(secondLargest(secondLargeArr), "Second largest");
+
+// Basic problem: Remove duplicates from Sorted Array
+// Input: arr[] = [2, 2, 2, 2, 2]
+// Output: [2]
+// Explanation: All the elements are 2, So only keep one instance of 2.
+// Input: arr[] = [1, 2, 2, 3, 4, 4, 4, 5, 5]
+// Output: [1, 2, 3, 4, 5]
+// Input: arr[] = [1, 2, 3]
+// Output: [1, 2, 3]
+// Explanation : No change as all elements are distinct.
+
+function removeDuplicates(arr) {
+   let n = arr.length;
+   if (n <= 0) {
+      return 0;
+   }
+   if (n === 1) return 1;
+
+   let i = 1;
+   for (j = 1; j < n; j++) {
+      if (arr[j] !== arr[j - 1]) {
+         arr[i] = arr[j];
+         i++;
+      }
+   }
+   return i;
+}
+
+const removeDuplicateArr = [1, 2, 2, 3, 4, 4, 4, 5, 5];
+const k = removeDuplicates(removeDuplicateArr);
+// console.log(removeDuplicates(removeDuplicateArr), "removeDuplicates array");
+// console.log(removeDuplicateArr.slice(0, k));
+
+// Basic problem 5: Generating All Sub-arrays
+// Input: arr[] = [1, 2, 3]
+// Output: [ [1], [1, 2], [2], [1, 2, 3], [2, 3], [3] ]
+
+// Input: arr[] = [1, 2]
+// Output: [ [1], [1, 2], [2] ]
+
+function getSubArray(arr) {
+   const n = arr.length;
+   const subArray = [];
+   for (i = 0; i < n; i++) {
+      for (j = i + 1; j <= n; j++) {
+         console.log(i, j, arr.slice(i, j))
+         subArray.push(arr.slice(i, j));
+      }
+   }
+   return subArray;
+}
+
+const subArray = [1, 2, 3];
+// console.log(getSubArray(subArray), "Sub array");
+
+// Basic problem 6: Array Reverse
+// Input: arr[] = [1, 4, 3, 2, 6, 5] 
+// Output:  [5, 6, 2, 3, 4, 1]
+// Explanation: The first element 1 moves to last position, the second element 4 moves to second-last and so on.
+
+function reverseArray(arr) {
+   let reverseArr = [];
+
+   // Simple approach
+   // for (i = arr.length - 1; i >= 0; i--) {
+   //    console.log(i);
+   //    reverseArr.push(arr[i]);
+   // }
+   // return reverseArr;
+
+   // Using single pointer
+   // const n = arr.length;
+   // for (i = 0; i < arr.length / 2; i++) {
+   //    const temp = arr[i];
+   //    arr[i] = arr[n - i - 1];
+   //    arr[n - i - 1] = temp;
+   // }
+   // return arr;
+
+   // Using two pointer
+   const n = arr.length;
+   let start = 0;
+   let end = n - 1;
+   while (start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+   }
+   return arr;
+}
+
+const arrayReverse = [1, 4, 3, 2, 6, 5];
+// console.log(reverseArray(arrayReverse), "Reverse array");
+
+// Basic problem: Rotate an Array - Clockwise or Right
+// Input: arr[] = {1, 2, 3, 4, 5, 6}, d = 2
+// Output: {5, 6, 1, 2, 3, 4}
+// Explanation: After first right rotation, arr[] becomes {6, 1, 2, 3, 4, 5}
+//  and after the second rotation, arr[] becomes {5, 6, 1, 2, 3, 4}
+
+function arrRotation(arr, d) {
+   let n = arr.length;
+   for (i = 0; i < d; i++) {
+      let last = arr[n - 1];
+      for (j = n - 1; j > 0; j--) {
+         arr[j] = arr[j - 1];
+      }
+      arr[0] = last;
+   }
+   return arr;
+}
+
+const rotateThis = [1, 2, 3, 4, 5, 6];
+console.log(arrRotation(rotateThis, 2), "Rotation of Array");
