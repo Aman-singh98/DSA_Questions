95b522a83bc313bb349c088691862d61314e7a15
Aman Singh <amansingh160798@gmail.com>
2026-05-30 23:22:49 +0530

    add slide widow cocept questiona

---
+++ Array/slide.js
@@ -0,0 +1,61 @@
+// Question 1: Max Sum Subarray of Size K
+// Input:
+// nums = [2, 1, 5, 1, 3, 2]
+// k = 3
+// Output:
+// 9
+
+// 2. Average of Every Subarray of Size K
+// Input:
+// nums = [1, 3, 2, 6, -1, 4, 1, 8, 2]
+// k = 5
+
+// Output:
+// [2.2, 2.8, 2.4, 3.6, 2.8]
+
+// 3. First Negative Integer in Every Window of Size K
+// Input:
+// nums = [12, -1, -7, 8, -15, 30, 16, 28]
+// k = 3
+
+// Output:
+// [-1, -1, -7, -15, -15, 0]
+
+// 4. Longest Substring Without Repeating Characters
+// Input:
+// s = "abcabcbb"
+
+// Output:
+// 3
+
+// 5. Longest Subarray with Sum ≤ K
+// Input:
+// nums = [1, 2, 1, 0, 1, 1, 0]
+// k = 4
+
+// Output:
+// 5
+
+// 6. Minimum Window Substring
+// Input:
+// s = "ADOBECODEBANC"
+// t = "ABC"
+
+// Output:
+// "BANC"
+
+// 7. Count Subarrays with Product Less Than K
+// Input:
+// nums = [10, 5, 2, 6]
+// k = 100
+
+// Output:
+// 8
+
+// 8. Max Consecutive Ones III
+// Input:
+// nums = [1,1,1,0,0,0,1,1,1,1,0]
+// k = 2
+
+// Output:
+// 6
