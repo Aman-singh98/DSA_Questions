d0eb5610f7f53ac002cdf4ce57baee88ca138f4e
Aman singh <amansingh160798@gmail.com>
2026-06-01 00:15:09 +0530

    Solve sliding widow

--- Array/slide.js
+++ Array/slide.js
@@ -4,22 +4,57 @@
 // k = 3
 // Output:
 // 9
+function findTheMaxSum(arr = [2, 1, 5, 1, 3, 2]) {
+   let k = 3;
+   let windowSum = 0;
+
+   for (let i = 0; i < k; i++) {
+      windowSum += arr[i];
+   }
+
+   let maxSum = windowSum;
+
+   for (let i = k; i < arr.length; i++) {
+      windowSum = windowSum - arr[i - k] + arr[i];
+      if (maxSum < windowSum) {
+         maxSum = windowSum;
+      }
+   }
+   return maxSum;
+}
+// console.log(findTheMaxSum(), "findTheMaxSum()");
 
 // 2. Average of Every Subarray of Size K
 // Input:
 // nums = [1, 3, 2, 6, -1, 4, 1, 8, 2]
 // k = 5
-
 // Output:
 // [2.2, 2.8, 2.4, 3.6, 2.8]
+function avreageofSubArray(arr = [1, 3, 2, 6, -1, 4, 1, 8, 2], k = 5) {
+   let sumofWindow = 0;
+   let result = [];
+   for (let i = 0; i < k; i++) {
+      sumofWindow += arr[i];
+   }
+   let average = sumofWindow / k;
+   result.push(average);
+   for (let i = k; i < arr.length; i++) {
+      sumofWindow = sumofWindow - arr[i - k] + arr[i];
+      average = sumofWindow / k;
+      result.push(average);
+   }
+   return result;
+}
+// console.log(avreageofSubArray(), "avreageofSubArray()");
 
 // 3. First Negative Integer in Every Window of Size K
 // Input:
 // nums = [12, -1, -7, 8, -15, 30, 16, 28]
 // k = 3
-
 // Output:
 // [-1, -1, -7, -15, -15, 0]
+
+
 
 // 4. Longest Substring Without Repeating Characters
 // Input:
