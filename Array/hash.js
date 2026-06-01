28e1101acc23774c44b977dce82a47d4fe4418c4
Aman singh <amansingh160798@gmail.com>
2026-06-01 23:27:13 +0530

    Solve hashing count duplicate problem

--- Array/hash.js
+++ Array/hash.js
@@ -9,3 +9,21 @@
 // 8. Hashing	Group anagrams together
 // 9. Hashing	Longest consecutive sequence
 // 10. Hashing	Total problems target
+console.log("solve hashing problems");
+
+function countFrequency(arr = [1, 2, 2, 3, 1, 1]) {
+   let mp = new Map();
+   arr?.forEach((elem) =>
+      mp.set(elem, (mp.get(elem) || 0) + 1)
+   );
+   console.log(Object.fromEntries(mp));
+   return mp;
+}
+// console.log(countFrequency());
+
+function isDuplicate(arr = [1, 2, 3, 1]) {
+   let mp = new Map();
+   arr.forEach((elem) => mp.set(elem, (mp.get(elem) || 0) + 1));
+   console.log(Object.fromEntries(mp));
+}
+console.log(isDuplicate());
