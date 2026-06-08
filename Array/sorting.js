7d6587a2027f7f19a0c9d95c46107e9fbf5ec264
Aman singh <amansingh160798@gmail.com>
2026-06-08 22:39:51 +0530

    Solve bubble sort question

--- Array/sorting.js
+++ Array/sorting.js
@@ -1,1 +1,15 @@
 console.log("Sorting Algor")
+// 1. Buble sort
+function bubbleSort(arr) {
+   let n = arr?.length;
+   for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+         if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+         }
+      }
+   }
+   return arr;
+}
+
+console.log(bubbleSort([5, 3, 8, 4]));
